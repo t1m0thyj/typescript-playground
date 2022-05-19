b762fcb7bdb7ec4e94f6a38a15e64af331952693
Timothy Johnson <timothy.johnson@broadcom.com>
2022-05-19 14:37:20 -0400

    Update dependencies and add readFile test
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,16 +1,26 @@
 import { spawnSync } from "child_process";
-import { writeLine } from "../src/index";
+import * as fs from "fs";
+import { readFile, writeLine } from "../src/index";
 
 const testLine = "The quick brown fox jumped over the lazy dog.";
 
 describe("Main program", () => {
+    it("readFile calls fs.readFileSync", () => {
+        const readFileSpy = jest.spyOn(fs, "readFileSync");
+        const fileContents = readFile("package.json");
+        expect(readFileSpy).toHaveBeenCalledTimes(1);
+        expect(fileContents.length).toBeGreaterThan(0);
+        readFileSpy.mockReset();
+    });
+
     it("writeLine calls console.log", () => {
         const logSpy = jest.spyOn(console, "log").mockImplementation();
         writeLine(testLine);
         expect(logSpy).toHaveBeenCalledTimes(1);
         expect(logSpy).toHaveBeenLastCalledWith(testLine);
         logSpy.mockReset();
-    }),
+    });
+
     it("prints hello world when run", () => {
         const output: string = spawnSync("node", ["dist/index.js"]).stdout.toString();
         expect(output.trim()).toBe("Hello, world!");
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import * as fs from "fs";
+
+export function readFile(filename: string): string {
+    return fs.readFileSync(__dirname + "/../" + filename, "utf-8");
+}
+
 export function writeLine(line: string) {
     console.log(line);
 }
